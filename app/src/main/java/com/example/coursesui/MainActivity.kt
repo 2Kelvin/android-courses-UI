5328c6202bf26fd7da42513e140ecad92574855d
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-21 15:21:30 +0300

    added first Row

--- app/src/main/java/com/example/coursesui/MainActivity.kt
+++ app/src/main/java/com/example/coursesui/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.coursesui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.coursesui.ui.theme.CoursesUITheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,72 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CoursesUI()
                 }
             }
         }
     }
 }
 
+//font mulish
+val mulish = FontFamily(
+    Font(R.font.mulish_medium, FontWeight.Normal),
+    Font(R.font.mulish_extra_light, FontWeight.ExtraLight)
+)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CoursesUI() {
+    Column(
+        Modifier
+            //.padding(start = 15.dp, end = 15.dp)
+            .fillMaxSize()
+            .background(colorResource(R.color.bg_purple))
+    ) {
+        // hey James
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp, start = 15.dp, end = 15.dp, bottom = 15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column() {
+                Text(
+                    text = "Hey James",
+                    color = Color.White,
+                    fontFamily = mulish,
+                    fontSize = 28.sp
+                )
+
+                Text(
+                    text = "Find what you want",
+                    fontFamily = mulish,
+                    fontSize = 28.sp,
+                    color = Color.White
+                )
+            }
+            Column() {
+                Card(
+                    shape = CircleShape,
+                    backgroundColor = colorResource(R.color.search_purple)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Search,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(30.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun CoursesUIPreview() {
     CoursesUITheme {
-        Greeting("Android")
+        CoursesUI()
     }
 }
